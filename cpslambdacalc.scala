1c6ba9393d0c284fbd3130255cc97580eba21f2c
Leif Andersen <leif@leifandersen.net>
2013-02-26 12:00:03 -0700

    Set to closures.

--- cpslambdacalc.scala
+++ cpslambdacalc.scala
@@ -19,18 +19,18 @@ case class LambExp(param: List[VarExp], body: CExp) extends AExp { }
 
 case class VarExp(value: String) extends AExp { }
 
-case class Closure(e: Exp, env: Map[VarExp, Exp]) extends Exp { }
+case class Closure(e: Exp, env: Map[VarExp, Closure]) extends Exp { }
 
 object Analysis {
 
-  def eval(e: Exp, env: Map[VarExp, Exp]): Exp = e match {
+  def eval(e: Exp, env: Map[VarExp, Closure]): Closure = e match {
     case ListExp(prog, arg) => apply(eval(prog, env), for(i <- arg) yield eval(i, env));
     case LambExp(param, body) => Closure(e, env);
     case a: VarExp => env(a);
   }
 
-  def apply(f: Exp, x: List[Exp]): Exp = f match {
-    case Closure(LambExp(param, body), env) => eval(body, env ++ (for(List(i: VarExp, j: Exp) <- param.zip(x)) yield (i -> j)));
+  def apply(f: Exp, x: List[Closure]): Closure = f match {
+    case Closure(LambExp(param, body), env) => eval(body, env ++ (for(List(i: VarExp, j: Closure) <- param.zip(x)) yield (i -> j)));
   }
 
   def main(args: Array[String]) {
