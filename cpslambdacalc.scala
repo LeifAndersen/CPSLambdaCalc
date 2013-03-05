1e4f37af78504624a2b0d3e9d9aa99ba244f4d83
Leif Andersen <leif@leifandersen.net>
2013-03-05 11:24:34 -0700

    Added store.

--- cpslambdacalc.scala
+++ cpslambdacalc.scala
@@ -9,7 +9,7 @@ abstract class Exp { }
 
 abstract class CExp extends Exp { }
 
-case class RunExp(prog: AExp, arg: List[AExp]) extends CExp { }
+case class ApplyExp(prog: AExp, arg: List[AExp]) extends CExp { }
 
 case class HaultExp() extends CExp { }
 
@@ -19,23 +19,40 @@ case class LambExp(param: List[VarExp], body: CExp) extends AExp { }
 
 case class VarExp(value: String) extends AExp { }
 
-case class Closure(e: Exp, env: Map[VarExp, Closure]) extends Exp { }
+case class Closure(e: Exp, env: Map[VarExp, Address]) extends Exp { }
+
+case class Address(address: Int) { }
 
 object Analysis {
 
-  var code = RunExp(LambExp(List(VarExp("x")), HaultExp()), List(VarExp("x")));
+  var code = ApplyExp(LambExp(List(VarExp("x")), HaultExp()), List(VarExp("x")));
 
-  def eval(e: Exp, env: Map[VarExp, Closure]): Closure = e match {
-    case RunExp(prog, arg) => apply(eval(prog, env), for(i <- arg) yield eval(i, env));
+  var store = Map[Address, Closure]();
+  var id = 0;
+
+  def extend(e: Closure): Address = {
+    val addr = Address(id);
+    store += (addr -> e);
+    id = id + 1;
+    return addr;
+  }
+
+  def lookup(e: Address): Closure = {
+    return store(e);
+  }
+
+  def eval(e: Exp, env: Map[VarExp, Address]): Closure = e match {
+    case ApplyExp(prog, arg) => apply(eval(prog, env), for(i <- arg) yield eval(i, env));
     case LambExp(param, body) => Closure(e, env);
-    case a: VarExp => env(a);
+    case a: VarExp => lookup(env(a));
+    case a: HaultExp => null;
   }
 
   def apply(f: Exp, x: List[Closure]): Closure = f match {
-    case Closure(LambExp(param, body), env) => eval(body, env ++ param.zip(x));
+    case Closure(LambExp(param, body), env) => eval(body, env ++ param.zip(for(i <- x) yield extend(i)));
   }
 
   def main(args: Array[String]) {
-    eval(code, Map[VarExp, Closure]());
+    eval(code, Map[VarExp, Address]());
   }
 }
