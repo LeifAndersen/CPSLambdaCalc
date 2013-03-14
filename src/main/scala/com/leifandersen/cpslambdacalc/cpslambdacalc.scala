801dd028cc0ccd967b59470489d641ed42c3f449
Leif Andersen <leif@leifandersen.net>
2013-03-14 15:22:40 -0600

    Explicit states now.

--- src/main/scala/com/leifandersen/cpslambdacalc/cpslambdacalc.scala
+++ src/main/scala/com/leifandersen/cpslambdacalc/cpslambdacalc.scala
@@ -22,19 +22,30 @@ case class LambExp(param: List[VarExp], body: CExp) extends AExp { }
 
 case class VarExp(value: String) extends AExp { }
 
-case class Closure(e: Exp, env: Map[VarExp, Address]) { }
+
+// case class Closure(e: Exp, env: Map[VarExp, Address]) { }
+
 
 case class Address(address: Int) { }
 
+
+abstract class State { }
+
+case class EvalState(e: Exp, env: Map[VarExp, Address]) extends State { }
+
+case class ApplyState(f: EvalState, x: List[EvalState]) extends State { }
+
+case class HaultState() extends State { }
+
 object Analysis extends App {
 
-  var store = Map[Address, Closure]();
+  var store = Map[Address, EvalState]();
   var id = 0;
 
-  var aStore = Map[Address, List[Closure]]();
+  var aStore = Map[Address, List[EvalState]]();
   val maxId = 10;
 
-  def aextend(e: Closure): Address = {
+  def aextend(e: EvalState): Address = {
     val addr = Address(id);
     if(aStore.contains(addr)) {
       aStore = aStore + (addr -> (aStore(addr) ::: List(e)));
@@ -45,30 +56,41 @@ object Analysis extends App {
     return addr;
   }
 
-  def alookup(e: Address): List[Closure] = {
+  def alookup(e: Address): List[EvalState] = {
     return aStore(e);
   }
 
-  def extend(e: Closure): Address = {
+  def extend(e: EvalState): Address = {
     val addr = Address(id);
     store += (addr -> e);
     id = id + 1;
     return addr;
   }
 
-  def lookup(e: Address): Closure = {
+  def lookup(e: Address): EvalState = {
     return store(e);
   }
 
-  def eval(e: Exp, env: Map[VarExp, Address]): Closure = e match {
-    case ApplyExp(prog, arg) => apply(eval(prog, env), for(i <- arg) yield eval(i, env));
-    case LambExp(param, body) => Closure(e, env);
+  def eval(e: Exp, env: Map[VarExp, Address]): EvalState = e match {
+    case ApplyExp(prog, arg) => step(ApplyState(eval(prog, env),
+                                                for(i <- arg) yield eval(i, env)));
+    case LambExp(param, body) => EvalState(e, env);
     case a: VarExp => lookup(env(a));
-    case a: HaultExp => null;
+    case a: HaultExp => step(HaultState());
   }
 
-  def apply(f: Closure, x: List[Closure]): Closure = f match {
-    case Closure(LambExp(param, body), env) => eval(body, env ++ param.zip(for(i <- x) yield extend(i)));
+  def apply(f: EvalState, x: List[EvalState]): EvalState = f match {
+    case EvalState(LambExp(param, body), env) => EvalState(body, env ++ param.zip(for(i <- x) yield extend(i)));
+  }
+
+  def step(state: State): EvalState = state match {
+    case EvalState(e: Exp, env: Map[VarExp, Address]) => eval(e, env);
+    case ApplyState(f: EvalState, x: List[EvalState]) => apply(f, x);
+    case HaultState() => null;
+  }
+
+  def inject(code: Exp): State = {
+    EvalState(code, Map[VarExp, Address]());
   }
 
   var code = ApplyExp(LambExp(List(VarExp("x")), VarExp("x")),
