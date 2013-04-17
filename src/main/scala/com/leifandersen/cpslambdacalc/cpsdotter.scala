a478bf9b5ff1231496a319423515d12a309afa48
Leif Andersen <leif@leifandersen.net>
2013-04-16 18:59:15 -0600

    4x speedup.

--- src/main/scala/com/leifandersen/cpslambdacalc/cpsdotter.scala
+++ src/main/scala/com/leifandersen/cpslambdacalc/cpsdotter.scala
@@ -4,5 +4,6 @@ import CPSLambdaCalc._
 import Scalify._
 
 object CPSDotter extends App {
-  println(analyzeNaiveAndGenerate(CodeSamples.fact5))
+//  println(analyzeNaiveAndGenerate(CodeSamples.omega))
+  println(analyzeAndGenerate(CodeSamples.omega))
 }
--- src/main/scala/com/leifandersen/cpslambdacalc/cpslambdacalc.scala
+++ src/main/scala/com/leifandersen/cpslambdacalc/cpslambdacalc.scala
@@ -71,14 +71,39 @@ object CPSLambdaCalc {
 
   def afix(in: Map[State,Set[State]]): Map[State,Set[State]] = {
     var next = in;
-    val producers = for(i <- in; j <- i._2; if (!next.contains(j))) yield
-      (j -> new StateProducer(j))
-    for(i <- producers.keys) {
-      var tmpStep = Set[State]();
-      for(j <- producers(i).iterator) {
+    var producers = new Iterator[(State,StateProducer)] {
+      var internal = List[(State,StateProducer)]()
+      def hasNext: Boolean = {
+        internal.length > 0
+      }
+      def next: (State, StateProducer) = {
+        val tmp = internal(0)
+        internal = internal.tail
+        return tmp
+      }
+      def extend(a: Set[(State,StateProducer)]) {
+        internal ++= a
+      }
+      def extendList(a: List[(State,StateProducer)]) {
+        internal ++= a
+      }
+    }
+    def getProducers(in: Set[State]) {
+      val t = for(j <- in; if (!next.contains(j))) yield
+        (j -> new StateProducer(j))
+      producers.extend(t)
+    }
+
+    for(i <- in) {
+      getProducers(i._2)
+    }
+    for(i <- producers) {
+      var tmpStep = Set[State]()
+      for(j <- i._2.iterator) {
         tmpStep ++= j
       }
-      next += (i -> tmpStep);
+      next += (i._1 -> tmpStep)
+      getProducers(tmpStep)
     }
     if(in == next) {
       return next;
